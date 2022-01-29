ce5a342b6e8cf4cbe1068ea06c403a0dffef606f
ashpakovskiy <shpakovskiyandrei@gmail.com>
2022-01-30 00:07:58 +0300

    Add chart and necessary math dependencies to view signal spectrum

--- src/main/java/com/shpakovskiy/soundanalyser/common/utils/sound/PageRetriever.java
+++ src/main/java/com/shpakovskiy/soundanalyser/common/utils/sound/PageRetriever.java
@@ -3,11 +3,14 @@ package com.shpakovskiy.soundanalyser.common.utils.sound;
 import com.shpakovskiy.soundanalyser.model.Sound;
 
 public class PageRetriever {
+
     //TODO: Think of better name
-    public static int[] retrieveSoundRange(Sound sound, int offset, int limit) {
+    public static double[] retrieveSoundRange(Sound sound, int offset, int limit) {
         int signalRangeLength = Math.min(sound.getRawValues().length - offset, limit);
-        int[] signalRange = new int[signalRangeLength];
+        double[] signalRange = new double[signalRangeLength];
+
         System.arraycopy(sound.getRawValues(), offset, signalRange, 0, signalRangeLength);
+
         return signalRange;
     }
 }
--- src/main/java/com/shpakovskiy/soundanalyser/model/Sound.java
+++ src/main/java/com/shpakovskiy/soundanalyser/model/Sound.java
@@ -3,9 +3,9 @@ package com.shpakovskiy.soundanalyser.model;
 public class Sound {
     private final float sampleRate;
     private final int sampleSizeBytes; //In some sources "Sample size" is called "Bit depth"
-    private final int[] rawValues;
+    private final double[] rawValues;
 
-    public Sound(float sampleRate, int sampleSizeBytes, int[] rawValues) {
+    public Sound(float sampleRate, int sampleSizeBytes, double[] rawValues) {
         this.sampleRate = sampleRate;
         this.sampleSizeBytes = sampleSizeBytes;
         this.rawValues = rawValues;
@@ -19,7 +19,7 @@ public class Sound {
         return sampleSizeBytes;
     }
 
-    public int[] getRawValues() {
+    public double[] getRawValues() {
         return rawValues;
     }
 }
