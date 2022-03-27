b0d1cf54ae07eadcd006f69b58db02f51ae46044
AndreiShpakovskiy <shpakovskiyandrei@gmail.com>
2022-03-28 00:34:53 +0300

    Add amplitude distribution view (experimental)

--- src/main/java/com/shpakovskiy/soundanalyser/common/utils/sound/PageRetriever.java
+++ src/main/java/com/shpakovskiy/soundanalyser/common/utils/sound/PageRetriever.java
@@ -2,6 +2,8 @@ package com.shpakovskiy.soundanalyser.common.utils.sound;
 
 import com.shpakovskiy.soundanalyser.model.Sound;
 
+import java.util.Map;
+
 public class PageRetriever {
 
     //TODO: Think of better name
@@ -12,5 +14,16 @@ public class PageRetriever {
         System.arraycopy(sound.getRawValues(), offset, signalRange, 0, signalRangeLength);
 
         return signalRange;
+    }
+
+    public static double[] getDistributionDensity(Sound sound) {
+        double[] distributionDensity = new double[Byte.MAX_VALUE * 2 + 1];
+
+        for (double currentAmplitude : sound.getRawValues()) {
+            //System.out.println(">>> AMP = " + currentAmplitude);
+            distributionDensity[(int) (Math.abs(currentAmplitude))]++;
+        }
+
+        return distributionDensity;
     }
 }
